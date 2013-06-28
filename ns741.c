3aa534d5a87566616373c6b1d6a1fa8e6e6cc10c
Andrey Chilikin <achilikin@gmail.com>
2013-06-28 20:32:01 +0100

    optimized rds processing

--- ns741.c
+++ ns741.c
@@ -13,7 +13,7 @@ char radiotext_text[64] = "                                                     
 // *hrr hrr*              "Twilight Sparkle is best Pony.                                  "
 
 /* 4 RDS 0A Groups containing Program service name */
-uint8_t station_frames[20][3] =
+static uint8_t station_frames[16][3] =
 {
 	{0x03, 0xE0, 0x00},
 	{0x05, 0x01, 0x24},
@@ -34,72 +34,56 @@ uint8_t station_frames[20][3] =
 	{0x05, 0x01, 0x27},
 	{0x05, 0xE0, 0xCD},
 	{0x05, ' ' , ' ' }, // 7  8
+};
 
-	// Radiotext
+// Radiotext
 /* 1 RDS 2A Group containing Radiotext chars */
+static uint8_t text_frames[4][3] =
+{
 	{0x03, 0xE0, 0x00},
 	{0x05, 0x21, 0x20},
 	{0x05, ' ' , ' ' },	// 1  2
 	{0x05, ' ' , ' ' },	// 3  4
-
 };
 
-void ns741_rds_set_radiotext(char *text)
+void ns741_rds_set_radiotext(const char *text)
 {
-	uint8_t i = 0, isend = 0;
-	for (i=0; i<64; i++)
+	uint8_t i;
+
+	// first copy text to our buffer
+	for (i = 0; *text && (i < 64); i++, text++)
 	{
-		if (text[i]==0)
-			isend = 1;
-		
-		if (!isend)
-			radiotext_text[i] = text[i];
-		else
-			radiotext_text[i] = ' ';
+		radiotext_text[i] = *text;
 	}
-	
-	//strcpy( radiotext_text, text );
+	// pad buffer with spaces
+	for (; i < 64; i++)
+	{
+		radiotext_text[i] = ' ';
+	}
 }
 
-void ns741_rds_set_progname(char *text)
+// text - up to 8 characters padded with zeros
+// zeros will be replaced with spaces for transmission
+// so exactly 8 chars will be transmitted
+void ns741_rds_set_progname(const char *text)
 {
-	station_frames[3][1] = text[0];
-	station_frames[3][2] = text[1];
-	station_frames[7][1] = text[2];
-	station_frames[7][2] = text[3];
-	station_frames[11][1]= text[4];
-	station_frames[11][2]= text[5];
-	station_frames[15][1]= text[6];
-	station_frames[15][2]= text[7];
+	station_frames[3][1] = text[0] ? text[0] : ' ';
+	station_frames[3][2] = text[1] ? text[1] : ' ';
+	station_frames[7][1] = text[2] ? text[2] : ' ';
+	station_frames[7][2] = text[3] ? text[3] : ' ';
+	station_frames[11][1]= text[4] ? text[4] : ' ';
+	station_frames[11][2]= text[5] ? text[5] : ' ';
+	station_frames[15][1]= text[6] ? text[6] : ' ';
+	station_frames[15][2]= text[7] ? text[7] : ' ';
 }
 
-int ns741_rds_start(void)
-{
-	return TWI_send_word(station_frames[0][0],
-			      station_frames[0][1],
-			      station_frames[0][2]
-			     );
-}
-
-void ns741_set_frequency(uint32_t f_khz)
-{
-	TWI_init();
-	/* calculate frequency in 8.192kHz steps*/
-	uint16_t val = (uint16_t)((uint32_t)f_khz*1000ULL/8192ULL);
-	TWI_send(0x0A, val);
-	TWI_send(0x0B, val >> 8);
-	//printf("\nSet frequency to  %lu(%lu) kHz \n", f_khz, val*8192UL/1000UL);
-	return;
-}
-
-// default register 0x00:
+// register 0x00 controls power and oscillator:
 //	bit 0 - power
 //	bit 1 - oscillator
-static uint8_t reg00h = 0x03;
 void ns741_power(uint8_t on)
 {
-	TWI_init();
-	if (on == 1)
+	uint8_t reg00h;
+	if (on)
 		reg00h = 0x03;	// power + oscillator are active
 	else
 		reg00h = 0x02;	// power is off
@@ -113,47 +97,48 @@ void ns741_power(uint8_t on)
  *
  * Thanks to Silvan König for init-sequence
  *************************************************************************/
+static uint8_t init_data[] =
+{
+	0x00, 0x02, 0x83, 0x0A, 
+	0x00, 0x00, 0x00, 0x00, 
+	0x7E, 0x0E, 0x08, 0x3F, 
+	0x2A, 0x0C, 0xE6, 0x3F, 
+	0x70, 0x0A, 0xE4, 0x00,
+	0x42, 0xC0, 0x41, 0xF4
+};
+
 int ns741_init(void)
 {
-	TWI_init();
-	
-	if (TWI_send(0x00, 0x00) == -1)
+	if ((TWI_init() == -1) || (TWI_send(0x00, 0x00) == -1))
 	{
 		return -1;
 	}
 
-	TWI_writeInitData();
+	TWI_writeData(init_data, sizeof(init_data));
 
-	TWI_send(0x02, 0x0b);
+	TWI_send(0x02, 0x0B);
 	TWI_send(0x15, 0x11);
 	TWI_send(0x10, 0xE0);
 
 	// Configuration
-	TWI_send(0x02, 0x0B);
 	TWI_send(0x07, 0x7E);
 	TWI_send(0x08, 0x0E);
 	TWI_send(0x02, 0xCA);	//0x0A Sendeleistung
 	TWI_send(0x01, 0x81);
 	
-	//Initialize RPI lib for RDS interrupt pin
-	if (!bcm2835_init())
-		return 1;
-	//set RDS interrupt pin as input
-	bcm2835_gpio_fsel(RDSINT, BCM2835_GPIO_FSEL_INPT);
 	return 0;
 }
 
-// default register 0x02: 
-//	RF output power 3 (bits 7-6)
+// register 0x02 controls output power and mute: 
+//	RF output power 0-3 (bits 7-6) corresponding to 0.5, 0.8, 1.0 and 2.0 mW
 //	reserved bits 1 & 3 are set
 //	mute is off (bit 0)
+// default: 0xCA
 static uint8_t reg02h = 0xCA;
 
 void ns741_mute(uint8_t on)
 {
-	TWI_init();
-
-	if (on == 1 ) {
+	if (on) {
 		reg02h |= 1;
 	}
 	else {
@@ -166,38 +151,73 @@ void ns741_mute(uint8_t on)
 
 void ns741_txpwr(uint8_t strength)
 {
-	TWI_init();
-
 	// clear RF power bits: set power level 0 - 0.5mW
 	reg02h &= ~0xC0;
 	strength &= 0x03; // just in case normalize strength
 	reg02h |= (strength << 6);
+
 	TWI_send(0x02, reg02h);
 
 	return;
 }
 
-void RDSINT_vect()
+void ns741_set_frequency(uint32_t f_khz)
+{
+	/* calculate frequency in 8.192kHz steps*/
+	uint16_t val = (uint16_t)((uint32_t)f_khz*1000ULL/8192ULL);
+
+	// it is recommended to mute transmitter before changing frequency
+	TWI_send(0x02, reg02h & 0xFE);
+
+	TWI_send(0x0A, val);
+	TWI_send(0x0B, val >> 8);
+
+	// restore previous mute state
+	TWI_send(0x02, reg02h);
+
+	//printf("\nSet frequency to  %lu(%lu) kHz \n", f_khz, val*8192UL/1000UL);
+	return;
+}
+
+// start RDS transmission
+int ns741_rds_start(void)
+{
+	return TWI_send_word(station_frames[0][0],
+		station_frames[0][1],
+		station_frames[0][2]
+	);
+}
+
+// in total we sent 80 frames: 16 frames with Station Name and 64 with Radiotext
+void ProcessRDS(void)
 {
 	static uint8_t frame_counter = 0;
 	static uint8_t radiotext_counter = 0;
-	uint8_t i = 0;
-	
-	if (frame_counter == 0)
-	{
-		station_frames[17][2] = 0x20 | radiotext_counter;
-		i = radiotext_counter<<2; // *4
-		station_frames[18][1] = radiotext_text[i];
-		station_frames[18][2] = radiotext_text[i+1];
-		station_frames[19][1] = radiotext_text[i+2];
-		station_frames[19][2] = radiotext_text[i+3];
-		
-		radiotext_counter = (radiotext_counter + 1) % 16;
+
+	// first 16 frames - Station Name
+	if (frame_counter < 16) {
+		TWI_send_word(station_frames[frame_counter][0],
+			station_frames[frame_counter][1],
+			station_frames[frame_counter][2]
+		);
 	}
-	TWI_send_word(station_frames[frame_counter][0],
-						  station_frames[frame_counter][1],
-					      station_frames[frame_counter][2]
-					      );
-	frame_counter = (frame_counter + 1) % 20;
+	else { // 16 to 79 - Radio Text frames
+		uint8_t i;
+		if ((frame_counter & 0x03) == 0) {
+			text_frames[1][2] = 0x20 | radiotext_counter;
+			i = radiotext_counter << 2; // *4
+			text_frames[2][1] = radiotext_text[i];
+			text_frames[2][2] = radiotext_text[i+1];
+			text_frames[3][1] = radiotext_text[i+2];
+			text_frames[3][2] = radiotext_text[i+3];
+			radiotext_counter = (radiotext_counter + 1) % 16;
+		}
+		i = frame_counter & 0x03;
+		TWI_send_word(text_frames[i][0],
+			text_frames[i][1],
+			text_frames[i][2]);
+	}
+
+	frame_counter = (frame_counter + 1) % 80;
 	return;
 }
