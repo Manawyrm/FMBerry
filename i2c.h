dcb57f9d5f389938ec34e3ce4f3e5fc80a764a2f
Andrey Chilikin <achilikin@gmail.com>
2014-12-06 12:26:14 +0000

    int i2c_writeData renamed to int i2c_send_data

--- i2c.h
+++ i2c.h
@@ -1,7 +1,7 @@
 /*
 	FMBerry - an cheap and easy way of transmitting music with your Pi.
     Copyright (C) 2011-2013 by Tobias Mädel (t.maedel@alfeld.de)
-	Copyright (C) 2013      by Andrey Chilikin (achilikin@gmail.com)
+	Copyright (C) 2013      by Andrey Chilikin (https://github.com/achilikin)
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -24,6 +24,6 @@ int i2c_init(uint8_t bus, uint8_t address);
 int i2c_select(int dev, uint8_t address);
 int i2c_send_word(int dev, uint8_t addr, uint8_t data0, uint8_t data1);
 int i2c_send(int dev, uint8_t addr, uint8_t data);
-int i2c_writeData(int dev, uint8_t *data, uint32_t len);
+int i2c_send_data(int dev, uint8_t *data, uint32_t len);
 
 #endif
